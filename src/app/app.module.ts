611eba65b0a2b72806bc0ee9a0f99955b6b18656
Daniele Iudica <102958741+DanieleIudica@users.noreply.github.com>
2022-12-30 14:28:24 +0100

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MyMaterialModule } from "./my-material/my-material.module";
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { TrainingComponent } from "./training/training.component";
+import { NewTrainingComponent } from "./training/new-training/new-training.component";
+import { PastTrainingsComponent } from "./training/past-trainings/past-trainings.component";
+import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        SignupComponent,
+        TrainingComponent,
+        NewTrainingComponent,
+        PastTrainingsComponent,
+        CurrentTrainingComponent,
+        WelcomeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MyMaterialModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

88f95ded683c8865e0d47ebb35c50b6e8e573511
Daniele Iudica <102958741+DanieleIudica@users.noreply.github.com>
2023-01-31 22:45:46 +0100

    sorting and filtering

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import { Exercise } from "../exercise.model";
 import { TrainingService } from "../training.service";
@@ -10,11 +11,27 @@ import { TrainingService } from "../training.service";
 })
 export class PastTrainingsComponent implements OnInit {
     displayedColumns = ["date", "name", "calories", "duration", "state"];
-    dataSource!: Exercise[];
+    dataSource!: MatTableDataSource<Exercise>;
+    @ViewChild(MatSort) sort!: MatSort;
 
     constructor(private trainingSrv: TrainingService) {}
 
+    pastTrainingsArr = this.trainingSrv.getCompleteOrCanecelledExercises();
+
     ngOnInit(): void {
-        this.dataSource = this.trainingSrv.getCompleteOrCanecelledExercises();
+        this.dataSource = new MatTableDataSource(this.pastTrainingsArr);
+    }
+
+    ngAfterViewInit(): void {
+        this.dataSource.sort = this.sort;
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        // if (this.dataSource.paginator) {
+        //   this.dataSource.paginator.firstPage();
+        // }
     }
 }
